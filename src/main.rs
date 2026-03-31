21bc4ed0366fb8ba9d3147e5ac178c1891d80b95
squarethebest <vyazazyy@gmail.com>
2026-03-31 22:59:10 +0400

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::io::{Read, Write};
+use rand::Rng;
+use reqwest::blocking::Client;
+use std::thread;
+use std::time::Duration;
+use std::fs;
+use std::fs::OpenOptions;
+
+
+
+fn gen_code(len: usize) -> String {
+    let charsiki = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+    let mut rng = rand::thread_rng();
+    
+    (0..len).map(|_| {
+        let i = rng.gen_range(0..charsiki.len());
+        charsiki.chars().nth(i).unwrap()
+    }).collect()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    loop {
+        let client = Client::new();
+
+        let code = gen_code(5);
+        println!("тест {code}");
+
+        let response = client.get(&format!("https://dropmefiles.com/{}", code)) // если писать https://dropmefiles.com/AxO19 то выводит + а если писать главную страницу то -
+            .send()?;
+
+
+        println!("Статус: {}", response.status());
+        
+        let resp = response.text()?;
+
+        if resp.contains("var UPLOADID =") {
+            println!("+");
+            println!("https://dropmefiles.com/{}", code);
+            let url = format!("https://dropmefiles.com/{}\n", code);
+            let mut file = OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open("good.txt")
+                .unwrap();
+            
+            file.write_all(url.as_bytes()).unwrap();
+            
+        } else {
+            println!("-");
+        }
+
+        thread::sleep(Duration::from_millis(2000));
+
+    }
+    Ok(())
+}
